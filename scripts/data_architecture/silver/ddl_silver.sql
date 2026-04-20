04d03d201e51b5596fcce60a0fa138f4db824542
Nathan Krauss <nwkrauss@gmail.com>
2026-04-20 18:27:53 -0400

    Update ddl_silver.sql

--- scripts/data_architecture/silver/ddl_silver.sql
+++ scripts/data_architecture/silver/ddl_silver.sql
@@ -8,21 +8,22 @@ Script Purpose:
     Run this script to re-define the DDL of the 'bronze' tables.
 =====================================================================================
 */
+
 DROP TABLE IF EXISTS silver.income;
 CREATE TABLE silver.income (
-	job_name VARCHAR(200),
+	job_name VARCHAR(500),
 	start_date DATE,
 	end_date DATE,
 	date_string VARCHAR(200),
 	start_date_if_equipment DATE,
 	num_days INT,
 	job_position VARCHAR(50),
-	rate VARCHAR(50),
+	rate INT,
 	per_hours INT,
 	notes VARCHAR(200),
-	gross_1099 VARCHAR(50),
-	gross_w2 VARCHAR(50),
-	net_w2 VARCHAR(50),
+	gross_1099 NUMERIC,
+	gross_w2 NUMERIC,
+	net_w2 NUMERIC,
 	tax_structure VARCHAR(10),
 	payroll_name VARCHAR(200),
 	between_date_1 DATE,
